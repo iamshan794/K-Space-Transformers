6334fdc9e5f39b5ac8723e319a1d8fcf3ce21f35
Nimisha-Pabbichetty <60477893+Nimisha-Pabbichetty@users.noreply.github.com>
2023-04-19 03:20:44 +0530

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -23,7 +23,7 @@ class TrainDataSet(Dataset):
     self.w = self.k_gt.shape[2]
 
   def __len__(self):
-    return self.image_num-50
+    return self.image_num-100
 
   def __getitem__(self, index):
 
@@ -172,7 +172,7 @@ class ValidDataSet(Dataset):
 
   def __len__(self):
 
-    return self.image_num
+    return self.image_num-20
 
   def __getitem__(self, index):
 
